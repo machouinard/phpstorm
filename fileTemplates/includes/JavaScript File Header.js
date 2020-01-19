5a545472ddc692fed9376bb3a15c24fc50968b16
Mark Chouinard <mark@chouinard.me>
2020-01-18 18:16:54 -0800

    PS-2019.3.2 Preview <mark@mark.local Create markdown.xml, IntelliLang.xml, print.xml, project.default.xml, vagrant.xml, Default.xml, file.template.settings.xml, find.xml, WPAlt.xml, code.style.schemes.xml, Zen HTML.xml, path.macros.xml, React Templates.xml, React.xml, Default.xml, PHP.xml, JavaScript.xml, WordPress.xml, AngularJS.xml, docker.xml, Default _2_.xml, Zen XSL.xml, keymap.xml, JavaScript File Header.js, editor.xml, .DS_Store, webServers.xml, debugger.xml, PHP File.php, updates.xml, baseRefactoring.xml, ide.general.xml, web-types-registry.xml, user.xml, git.xml, Zen CSS.xml, Twig.xml, WordPress.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, php.xml, remote-servers.xml, xsl.xml, JavaScript Testing.xml, watcherDefaultTasks.xml, colors.scheme.xml, html_xml.xml, notifications.xml, databaseDrivers.xml, WPAlt copy.xml, github.xml, templates.xml, Vue.xml, Shell Script.xml, SQL.xml, PHP Function Doc Comment.php, Angular.xml, parameter.hints.xml, jdk.table.xml, Monokai copy.icls, PHP File Header.php, laf.xml, Default _1_.xml, web-browsers.xml, HTTP Request.xml, vcs.xml, databaseSettings.xml, PHP Interfaces.xml, IntelliJ IDEA Classic _OS X_ copy.xml, usageView.xml

---
+++ fileTemplates/includes/JavaScript File Header.js
@@ -0,0 +1,8 @@
+/**
+* Description
+*
+* @package      ${NAMESPACE}
+* @since        1.0.0
+* @author       machouinard
+* @link         https://chouinard.me
+*/
---
+++ fileTemplates/includes/PHP Function Doc Comment.php
@@ -0,0 +1,10 @@
+/**
+* Description
+*
+* @since 1.0.0
+*
+#if (${PARAM_DOC} != '') ${PARAM_DOC}
+*
+#end
+* @return ${TYPE_HINT}
+*/
